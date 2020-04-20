c2e0a986b532ebb096545b5f703214bbfeb329fa
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-04-20 18:25:19 +0200

    also remove c_api_only logic in test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,8 +8,6 @@ class TestPackageConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        if not self.options["spirv-tools"].c_api_only:
-            cmake.definitions["BUILD_CPP_TEST_PACKAGE"] = True
         cmake.configure()
         cmake.build()
 
@@ -17,6 +15,5 @@ class TestPackageConan(ConanFile):
         if not tools.cross_building(self.settings):
             bin_path_c = os.path.join("bin", "test_package_c")
             self.run(bin_path_c, run_environment=True)
-            if not self.options["spirv-tools"].c_api_only:
-                bin_path_cpp = os.path.join("bin", "test_package_cpp")
-                self.run(bin_path_cpp, run_environment=True)
+            bin_path_cpp = os.path.join("bin", "test_package_cpp")
+            self.run(bin_path_cpp, run_environment=True)
