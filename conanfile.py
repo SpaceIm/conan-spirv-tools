cf308a017c554538af3ef53d23c6c626020b2038
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-11-05 09:03:51 +0100

    minor change

--- conanfile.py
+++ conanfile.py
@@ -125,7 +125,8 @@ class SpirvtoolsConan(ConanFile):
         cmake.definitions["SPIRV_SKIP_EXECUTABLES"] = not self.options.build_executables
 
         # Avoid configuration error if building for iOS/tvOS/watchOS
-        cmake.definitions["CMAKE_MACOSX_BUNDLE"] = False
+        if tools.is_apple_os(self.settings.os):
+            cmake.definitions["CMAKE_MACOSX_BUNDLE"] = False
 
         cmake.configure(build_folder=self._build_subfolder)
         self._cmake = cmake
